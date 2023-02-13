11960041b3fd43fb734f687e98bbbb8c9a7313ae
dbarrow257 <danbarrow257@gmail.com>
2023-02-13 20:38:44 +0000

    Fully working set of scripts to install all tools for both versions of the code

---
+++ SKReprodInstallScripts/setup_Deps.sh
@@ -0,0 +1,12 @@
+#imake and makedepend
+export PATH=$PATH:/opt/ppd/t2k/users/barrowd/Software/imake/imake-build/bin
+export PATH=$PATH:/opt/ppd/t2k/users/barrowd/Software/makedepend/makedepend-build/bin
+
+#CERNLIB
+export PATH=$PATH:/opt/ppd/t2k/users/barrowd/T2KSoftware/CERNLIB/2005/bin
+export CERN=/opt/ppd/t2k/users/barrowd/T2KSoftware/CERNLIB
+export CERN_ROOT=/opt/ppd/t2k/users/barrowd/T2KSoftware/CERNLIB/2005
+export CERN_LEVEL=2005
+
+#ROOT
+source /opt/ppd/t2k/users/barrowd/Software/Root/root_v5_34_38-Build/bin/thisroot.sh
--- SKReprodInstallScripts/setup_T2K.sh
+++ SKReprodInstallScripts/setup_T2K.sh
@@ -1,17 +1,5 @@
 export T2KUtils=$PWD
-
-#Why oh god does NEUT have to use CERNLIB which in turn uses imake and makedepend????
-export PATH=$PATH:/opt/ppd/t2k/users/barrowd/Software/imake/imake-build/bin
-export PATH=$PATH:/opt/ppd/t2k/users/barrowd/Software/makedepend/makedepend-build/bin
-
-#CERNLIB
-export PATH=$PATH:/opt/ppd/t2k/users/barrowd/T2KSoftware/CERNLIB/2005/bin
-export CERN=/opt/ppd/t2k/users/barrowd/T2KSoftware/CERNLIB
-export CERN_ROOT=/opt/ppd/t2k/users/barrowd/T2KSoftware/CERNLIB/2005
-export CERN_LEVEL=2005
-
-#ROOT
-source /opt/ppd/t2k/users/barrowd/Software/Root/root_v5_34_38-Build/bin/thisroot.sh
+source setup_Deps.sh
 
 #NEUT
 export NEUT_ROOT=${T2KUtils}/neut/
