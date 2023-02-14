022eff715a0e520f4a84bd9923a95c446e27484e
dbarrow257 <danbarrow257@gmail.com>
2023-02-14 09:42:01 +0000

    Checkout correct XsecResponse for beam samples in SKReprod

--- SKReprodInstallScripts/build_xsecresp_t2ksamples.sh
+++ SKReprodInstallScripts/build_xsecresp_t2ksamples.sh
@@ -2,7 +2,7 @@ export XSECRS_DIR=XsecResponse_T2K
 
 git clone git@github.com:/t2k-software/XsecResponse ${XSECRS_DIR}
 cd ${XSECRS_DIR}
-git checkout 9b9812e #SK_reprodofOA2020
+git checkout 383ef862 #DBarrow_JointFit_SKReprod_Beam
 
 make
 
