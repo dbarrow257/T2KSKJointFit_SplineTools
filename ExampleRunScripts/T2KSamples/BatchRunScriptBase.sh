8ecdf3dbcac206dc37d91363b8f90088ff30b827
dbarrow257 <danbarrow257@gmail.com>
2023-02-14 06:52:43 +0000

    Add example run code for beam samples

---
+++ ExampleRunScripts/T2KSamples/BatchRunScriptBase.sh
@@ -0,0 +1,42 @@
+#! /bin/bash
+
+T2KUtilDir="/opt/ppd/t2k/users/barrowd/T2KSoftware/SKReprod/"
+XsecRespDir="/opt/ppd/t2k/users/barrowd/T2KSoftware/SKReprod/XsecResponse_T2K/"
+
+MTupleDir="/opt/ppd/t2k/users/barrowd/T2KSoftware/SKReprod/T2KMtuples/"
+MTuplePrefix="t2ksk19b.fqv4r0."
+MTupleSuffix=".root"
+
+WtDir="/opt/ppd/t2k/users/barrowd/T2KSoftware/SKReprod/T2KReWeightOutput/T2K/"
+WtPrefix="t2ksk19b.fqv4r0."
+WtSuffix="_T2KReWeight_Weights.root"
+
+SplineDir="/opt/ppd/t2k/users/barrowd/T2KSoftware/SKReprod/XsecResponseOutput/T2K/"
+SplinePrefix="t2ksk19b.fqv4r0."
+SplineSuffix="_XsecResponse_Splines.root"
+
+SampleNum=SAMPLE
+ChannelNum=CHANNEL
+
+declare -a SampleSelec=("nue" "numu" "cc1pi" "nue" "numu")
+
+declare -a SampleSuffix=("nueselec" "numuselec" "cc1piselec" "nueselec" "numuselec")
+declare -a SamplePrefix=("fhc." "fhc." "fhc." "rhc." "rhc.")
+declare -a ChannelName=("nuebar_x_nuebar_" "nue_x_nue_" "numubar_x_nuebar_" "numu_x_nue_" "numubar_x_numubar_" "numu_x_numu_")
+
+cd ${T2KUtilDir}
+source ${T2KUtilDir}/setup_T2K.sh
+
+MTupleName=${MTuplePrefix}${SamplePrefix[SampleNum]}${ChannelName[ChannelNum]}${SampleSuffix[SampleNum]}
+
+MTupleFile=${MTupleDir}${MTupleName}${MTupleSuffix}
+WtFile=${WtDir}${MTupleName}${WtSuffix}
+SplineFile1D=${SplineDir}${MTupleName}"_1D"${SplineSuffix}
+SplineFile2D=${SplineDir}${MTupleName}"_2D"${SplineSuffix}
+
+cd ${T2KUtilDir}
+./OAGenWeightsApps/build/Linux/bin/genWeightsSKsplines_newfunctions_2021Weights -s ${MTupleFile} -o ${WtFile}
+cd ${XsecRespDir}
+bin/make_xsec_response_sk_2021 -m ${MTupleFile} -w ${WtFile} -o ${SplineFile1D} -selec ${SampleSelec[SampleNum]}
+bin/make_xsec_response_2021_2d -m ${MTupleFile} -w ${WtFile} -o ${SplineFile2D} -selec ${SampleSelec[SampleNum]} -f true
+
---
+++ ExampleRunScripts/T2KSamples/BatchSubmitScriptBase.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+Executable    = /opt/ppd/t2k/users/barrowd/T2KSoftware/SKReprod/BatchFiles_T2K/BatchRunScript_SAMPLE_CHANNEL.sh
+Output        = /opt/ppd/t2k/users/barrowd/T2KSoftware/SKReprod/BatchFiles_T2K/BatchRunScript_SAMPLE_CHANNEL.out
+Error         = /opt/ppd/t2k/users/barrowd/T2KSoftware/SKReprod/BatchFiles_T2K/BatchRunScript_SAMPLE_CHANNEL.err
+
+Queue 1
