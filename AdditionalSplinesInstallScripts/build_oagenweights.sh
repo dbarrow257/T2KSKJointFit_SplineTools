4b9232eb81fb585878e4bbad215abb4cf5f94e08
dbarrow257 <danbarrow257@gmail.com>
2023-02-20 20:34:42 +0000

    Update version of OAGenWeightsApps which is pulled for AdditionalSplines

--- AdditionalSplinesInstallScripts/build_oagenweights.sh
+++ AdditionalSplinesInstallScripts/build_oagenweights.sh
@@ -2,7 +2,7 @@ export OAGEN_DIR=OAGenWeightsApps
 
 git clone git@github.com:t2k-software/OAGenWeightsApps ${OAGEN_DIR}
 cd ${OAGEN_DIR}
-git checkout 9372c958d21f4c6c28b41c7eee3f3c4bb605565d #DBarrow_JointFit
+git checkout ebeded1d70069904100954b02bb7738ab5b97462 #DBarrow_JointFit
 
 mkdir build; cd build;
 cmake ../ -DUSE_SK=ON
