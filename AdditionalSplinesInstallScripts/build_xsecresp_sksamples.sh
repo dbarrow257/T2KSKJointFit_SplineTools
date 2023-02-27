25793e53962c07d54f79c6ffbdbd74912b5b0c01
dbarrow257 <danbarrow257@gmail.com>
2023-02-27 18:58:37 +0000

    Increment XsecResp builds in AdditionalSplines to build AdlerAngle dials in different momentum splits

--- AdditionalSplinesInstallScripts/build_xsecresp_sksamples.sh
+++ AdditionalSplinesInstallScripts/build_xsecresp_sksamples.sh
@@ -2,7 +2,7 @@ export XSECRS_DIR=XsecResponse_SK
 
 git clone git@github.com:/t2k-software/XsecResponse ${XSECRS_DIR}
 cd ${XSECRS_DIR}
-git checkout d91720b91faa2088ad566430101c1dd54914351e #DBarrow_JointFit_AdditionalSplines_Atm
+git checkout 0ebf7f537c67f119bc5e695f0140ed5b6f018561 #DBarrow_JointFit_AdditionalSplines_Atm
 
 make
 
--- AdditionalSplinesInstallScripts/build_xsecresp_t2ksamples.sh
+++ AdditionalSplinesInstallScripts/build_xsecresp_t2ksamples.sh
@@ -2,7 +2,7 @@ export XSECRS_DIR=XsecResponse_T2K
 
 git clone git@github.com:/t2k-software/XsecResponse ${XSECRS_DIR}
 cd ${XSECRS_DIR}
-git checkout c4400f32 #DBarrow_JointFit_AdditionalSplines_Beam
+git checkout 779a6929794692fe03e093ec5755674621a88450 #DBarrow_JointFit_AdditionalSplines_Beam
 
 make
 
