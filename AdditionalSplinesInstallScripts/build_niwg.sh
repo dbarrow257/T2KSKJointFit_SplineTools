e6440250d25ce517d95af5140159e11b7c81d41a
dbarrow257 <danbarrow257@gmail.com>
2023-02-27 18:41:29 +0000

    Move to NIWGRW which includes split adlerangle dial by momentum

--- AdditionalSplinesInstallScripts/build_niwg.sh
+++ AdditionalSplinesInstallScripts/build_niwg.sh
@@ -2,7 +2,7 @@ export NIWG_DIR=NIWGReWeight
 
 git clone git@github.com:/t2k-software/NIWGReWeight ${NIWG_DIR}
 cd ${NIWG_DIR}
-git checkout fafbb9fcb4535c3ac46cfe2f7d63df80cf2beab5 #DBarrow_JointFit_AdditionalSplines
+git checkout 517cf9d6328e6a0a3f0cfbd6bb1d7e9a8803ea91 #DBarrow_JointFit_AdditionalSplines
 
 mkdir build; cd build; 
 cmake ../ -DUSE_NEUT=1
